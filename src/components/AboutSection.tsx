f7dbf4b472254c880dfd51a56c8a8bb8e84525b6
Sindhu <sindhureddyvari3@gmail.com>
2024-09-28 19:19:14 +0530

    Added Projects

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -14,7 +14,7 @@ function AboutSection() {
       />
       <div className={styles.textdiv}>
         <h1>About me</h1>
-        <p>💻 I am a Final year Computer Science student from <b>Institute of Aeronautical Engineering,Hyderabad </b>, passionate about full stack web development and programming.</p>
+        <p>💻 I am a Final year Computer Science student from <b>Institute of Aeronautical Engineering,Hyderabad </b>. passionate about full stack web development and programming.</p>
         <p>🌟 I love building web apps and tackling challenges. Currently, I&apos;m honing my skills in HTML, CSS, JavaScript, MERN stack, Next.js, and TypeScript. Currently, I&apos;m exploring Docker and AWS.</p>
         <p>🌟My commitment to clean, efficient code ensures easy maintenance and scalability. </p>
         <p>🔍 Seeking internship opportunities in web development to further enhance skills and gain hands-on experience.</p>
