9c72710f991a44fdd7d47aca33265a9a75868686
Sindhu <sindhureddyvari3@gmail.com>
2024-09-28 18:47:18 +0530

    modified About Section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -14,8 +14,8 @@ function AboutSection() {
       />
       <div className={styles.textdiv}>
         <h1>About me</h1>
-        <p>💻 I am a Pre-final year Computer Science student from Institute of Aeronautical Engineering, passionate about full stack web development and programming.</p>
-        <p>🌟 I love building web apps and tackling challenges. Currently, I&apos;m honing my skills in HTML, CSS, JavaScript, MERN stack, Next.js, and TypeScript. Additionally, I&apos;m exploring Docker and AWS for both development and deployment.</p>
+        <p>💻 I am a Final year Computer Science student from Institute of Aeronautical Engineering, passionate about full stack web development and programming.</p>
+        <p>🌟 I love building web apps and tackling challenges. Currently, I&apos;m honing my skills in HTML, CSS, JavaScript, MERN stack, Next.js, and TypeScript. Currently, I&apos;m exploring Docker and AWS.</p>
         <p>🌟My commitment to clean, efficient code ensures easy maintenance and scalability. </p>
         <p>🔍 Seeking internship opportunities in web development to further enhance skills and gain hands-on experience.</p>
       </div>
