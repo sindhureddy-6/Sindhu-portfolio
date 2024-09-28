dff6153dde48d63eb764a13f2085f9488ffd02d2
Sindhu <sindhureddyvari3@gmail.com>
2024-09-28 19:18:19 +0530

    Added Projects

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -47,7 +47,7 @@ const Projects = () => {
             <div className={styles.projectcard}>
              <div><h1>Wanderlust-Airbnb clone</h1></div> 
               <div><p>Wanderlust: An Airbnb clone developed with MongoDB, Node.js, and Express.js, providing a comprehensive collection of hotel listings.</p></div>
-              <div><p>Skills-HTML, CSS, JavaScript, Node.js, MongoDB, Express</p></div>
+              <div><p>Skills-HTML, CSS, JavaScript, Node.js, MongoDB, Express.js</p></div>
 
               <div>
                 <span><a href="https://github.com/sindhureddy-6/Wanderlust">GitHub</a></span>
@@ -59,8 +59,8 @@ const Projects = () => {
           <SwiperSlide>
             <div className={styles.projectcard}>
              <div><h1>Edu-World</h1></div> 
-              <div><p>The Learning Management System (LMS) is an online platform designed to facilitate learning interactions between educators and students.[credentials:User&password:abcde]</p></div>
-              <div><p>Skills-HTML, CSS, JavaScript, Node.js, PostgresSQl, Express</p></div>
+              <div><p>The Learning Management System (LMS) is an online platform designed to facilitate learning interactions between educators and students.</p></div>
+              <div><p>Skills-HTML, CSS, JavaScript, Node.js, PostgresSQl, Express.js</p></div>
 
               <div>
                 <span><a href="https://github.com/sindhureddy-6/Learning-Management-System">GitHub</a></span>
@@ -82,36 +82,34 @@ const Projects = () => {
               
             </div>
           </SwiperSlide>
-          {/* <SwiperSlide>
+          <SwiperSlide>
             <div className={styles.projectcard}>
-              <h1>Project 4</h1>
+             <div><h1> Smarter Tasks</h1></div> 
+              <div><p>”Smarter Tasks,” a project management system.Integrated PupilFirst API for backend services to manage tasks and data flow. </p></div>
+              <div><p>Skills-Reactjs,TypeScript</p></div>
+
+              <div>
+                <span><a href="https://github.com/sindhureddy-6/WD301/tree/main/smarter-tasks">GitHub</a></span>
+                <span><a href="https://sm-tasks.netlify.app/">Live Site</a></span>
+              </div>
+              
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className={styles.projectcard}>
-              <h1>Project 5</h1>
+             <div><h1>Banking Management System</h1></div> 
+              <div><p>A CLI-based banking management System with features such as user registration, account management, and
+secure transactions. </p></div>
+              <div><p>Skills-Java, JDBC, MySQL</p></div>
+
+              <div>
+                <span><a href="https://github.com/sindhureddy-6/JDBC_PROJECT">GitHub</a></span>
+                <span><a href="https://github.com/sindhureddy-6/JDBC_PROJECT">Live Site</a></span>
+              </div>
+              
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.projectcard}>
-              <h1>Project 6</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.projectcard}>
-              <h1>Project 7</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.projectcard}>
-              <h1>Project 8</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.projectcard}>
-              <h1>Project 9</h1>
-            </div>
-          </SwiperSlide> */}
+          
         </Swiper>
       </div>
     </div>
